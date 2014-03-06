8243785ec362dbc3e43e63f858843b59f05783c0
Godefroid Chapelle <gotcha@bubblenet.be>
2014-03-06 15:43:57 +0100

    first import

---
+++ src/imio/facetednavigation/testing.py
@@ -0,0 +1,43 @@
+from plone.app.testing import PloneSandboxLayer
+from plone.app.testing import applyProfile
+from plone.app.testing import PLONE_FIXTURE
+from plone.app.testing import IntegrationTesting
+from plone.app.testing import FunctionalTesting
+
+from plone.testing import z2
+
+from zope.configuration import xmlconfig
+
+
+class ImiofacetednavigationLayer(PloneSandboxLayer):
+
+    defaultBases = (PLONE_FIXTURE,)
+
+    def setUpZope(self, app, configurationContext):
+        # Load ZCML
+        import imio.facetednavigation
+        xmlconfig.file(
+            'configure.zcml',
+            imio.facetednavigation,
+            context=configurationContext
+        )
+
+        # Install products that use an old-style initialize() function
+        #z2.installProduct(app, 'Products.PloneFormGen')
+
+#    def tearDownZope(self, app):
+#        # Uninstall products installed above
+#        z2.uninstallProduct(app, 'Products.PloneFormGen')
+
+    def setUpPloneSite(self, portal):
+        applyProfile(portal, 'imio.facetednavigation:default')
+
+IMIO_FACETEDNAVIGATION_FIXTURE = ImiofacetednavigationLayer()
+IMIO_FACETEDNAVIGATION_INTEGRATION_TESTING = IntegrationTesting(
+    bases=(IMIO_FACETEDNAVIGATION_FIXTURE,),
+    name="ImiofacetednavigationLayer:Integration"
+)
+IMIO_FACETEDNAVIGATION_FUNCTIONAL_TESTING = FunctionalTesting(
+    bases=(IMIO_FACETEDNAVIGATION_FIXTURE, z2.ZSERVER_FIXTURE),
+    name="ImiofacetednavigationLayer:Functional"
+)
---
+++ src/imio/facetednavigation/tests/test_robot.py
@@ -0,0 +1,13 @@
+from  imio.facetednavigation.testing import IMIO_FACETEDNAVIGATION_FUNCTIONAL_TESTING
+from plone.testing import layered
+import robotsuite
+import unittest
+
+
+def test_suite():
+    suite = unittest.TestSuite()
+    suite.addTests([
+        layered(robotsuite.RobotTestSuite("robot_test.txt"),
+                layer=IMIO_FACETEDNAVIGATION_FUNCTIONAL_TESTING)
+    ])
+    return suite
