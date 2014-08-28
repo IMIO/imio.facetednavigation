cdee7240373db7757499e8cce7a22e5ba299287c
Martin Peeters <martin.peeters@affinitic.be>
2014-08-28 17:36:12 +0200

    Add the test widget

---
+++ src/imio/facetednavigation/widgets/test/widget.py
@@ -0,0 +1,9 @@
+# encoding: utf-8
+
+from eea.facetednavigation.widgets.radio.widget import Widget as RadioWidget
+
+
+class Widget(RadioWidget):
+
+    widget_type = 'test'
+    widget_label = 'Test'
